330ffe4ce47ba67eb0a6a9d510fc9e73a24370d1
Tan Nguyen <tannp27@gmail.com>
2023-08-30 10:31:33 +0700

    update handle to run install latest php

---
+++ setup/develop/web/php/php-handler.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PHP_VERSION=$(curl -s https://www.php.net/downloads | grep -oP 'PHP [0-9]+\.[0-9]+' | head -1 | awk '{print $2}')
+
+phpExtensions() {
+    sudo apt install php"$PHP_VERSION" php"$PHP_VERSION"-{bcmath,common,fpm,xml,mysql,zip,intl,ldap,gd,bz2,curl,mbstring,pgsql,opcache,soap,redis} -y
+    sudo systemctl enable php"$PHP_VERSION"-fpm
+    sudo systemctl start php"$PHP_VERSION"-fpm
+}
+
+phpExtensions
--- setup/develop/web/php/php.sh
+++ setup/develop/web/php/php.sh
@@ -8,15 +8,7 @@ if ! command -v $COMMAND_NAME &>/dev/null; then
 
     echo "*****************"
     echo 'Auto checking for latest version...'
-    PHP_VERSION=$(curl -s https://www.php.net/downloads | grep -oP 'PHP [0-9]+\.[0-9]+' | head -1 | awk '{print $2}')
-
-    phpExtensions() {
-        sudo apt install php"$PHP_VERSION" php"$PHP_VERSION"-{bcmath,common,fpm,xml,mysql,zip,intl,ldap,gd,bz2,curl,mbstring,pgsql,opcache,soap,redis} -y
-        sudo systemctl enable php"$PHP_VERSION"-fpm
-        sudo systemctl start php"$PHP_VERSION"-fpm
-    }
-
-    phpExtensions
+    bash php-handler.sh
 else
     echo "$COMMAND_NAME install ok installed"
 fi
