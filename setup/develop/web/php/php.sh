ad71979ab977fe6536878d2c976d0fc5ab97aa8f
Tan Nguyen <tannp27@gmail.com>
2023-08-30 10:23:26 +0700

    add install latest php

---
+++ setup/develop/web/php/php.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+COMMAND_NAME="php"
+if ! command -v $COMMAND_NAME &>/dev/null; then
+    sudo apt install lsb-release gnupg2 ca-certificates apt-transport-https software-properties-common -y
+    sudo add-apt-repository ppa:ondrej/php
+
+    echo "*****************"
+    echo 'Auto checking for latest version...'
+    PHP_VERSION=$(curl -s https://www.php.net/downloads | grep -oP 'PHP [0-9]+\.[0-9]+' | head -1 | awk '{print $2}')
+
+    phpExtensions() {
+        sudo apt install php"$PHP_VERSION" php"$PHP_VERSION"-{bcmath,common,fpm,xml,mysql,zip,intl,ldap,gd,cli,bz2,curl,mbstring,pgsql,opcache,soap,cgi,redis} -y
+        sudo systemctl enable php"$PHP_VERSION"-fpm
+        sudo systemctl start php"$PHP_VERSION"-fpm
+    }
+else
+    echo "$COMMAND_NAME install ok installed"
+fi
