52fabfde6de9e1431b889d05b69f584a4e3a1462
Tan Nguyen <tannp27@gmail.com>
2023-08-30 16:24:00 +0700

    fix the condition in change ssl port and change form type

--- setup/system/change-port.sh
+++ setup/system/change-port.sh
@@ -5,16 +5,15 @@ echo '############## Change ssh port from 22 to your type ################'
 
 while true; do
     if [[ $ACCEPT_INSTALL =~ ^[Yy]$ ]]; then
-        ynInstall="n"
+        yn="n"
     else
-        read -r "Do you want to change ssh port? (Y/N)  " ynInstall
+        read -r -p "Do you want to change ssh port? (Y/N)  " yn
     fi
 
-    case $ynInstall in
+    case $yn in
     [Yy]*)
         echo "=========================== ssh ==========================="
-        echo "Please enter your new ssh port: "
-        read -r new_port
+        read -r -p "Please enter your new ssh port:  " new_port
         sudo sed -i "s/#Port [0-9]*/Port $new_port/g" /etc/ssh/sshd_config
         sudo sed -i "s/Port [0-9]*/Port $new_port/g" /etc/ssh/sshd_config
         sudo systemctl restart sshd
