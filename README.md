ed0e8bccb422a796e95208a2e5fff8ab981a5936
Tan Nguyen <tannp27@gmail.com>
2023-12-11 02:59:31 +0700

    update packages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dotfiles_server
+
+This script is used to setup a new server with my dotfiles.
--- setup/packages/nvm.sh
+++ setup/packages/nvm.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 COMMAND_NAME="nvm"
-if ! command -v $COMMAND_NAME &>/dev/null; then
+if [ ! -s "$HOME/.nvm/nvm.sh" ]; then
     echo "$COMMAND_NAME could not be found. Setting up $COMMAND_NAME."
     curl https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
     # shellcheck disable=SC1090
     source ~/.bashrc
 
@@ -14,3 +15,8 @@ else
     echo "$COMMAND_NAME install ok installed"
 fi
 echo ""
+
+if [ ! -s "$HOME/.nvm/nvm.sh" ]; then
+    echo "=========================== npm ==========================="
+    bash npm.sh
+fi
