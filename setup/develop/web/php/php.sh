0faca7b1de9ff109f1f5765970e2c5ed02d5671a
Tan Nguyen <tannp27@gmail.com>
2024-08-28 22:26:27 +0700

    :+1: feat: add update package in php handling

--- setup/develop/web/php/php.sh
+++ setup/develop/web/php/php.sh
@@ -3,8 +3,10 @@
 COMMAND_NAME="php"
 if ! command -v $COMMAND_NAME &>/dev/null; then
     echo "=========================== php ==========================="
+    sudo apt update
     sudo apt install lsb-release gnupg2 ca-certificates apt-transport-https software-properties-common -y
     sudo add-apt-repository ppa:ondrej/php
+    sudo apt update
 
     echo "*****************"
     echo 'Auto checking for latest version...'
